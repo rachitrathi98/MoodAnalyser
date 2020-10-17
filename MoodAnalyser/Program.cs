1242fc8a6d3965e1847b254ae4d0fc2bba08c553
Rachit Rathi <rachitrathi98@gmail.com>
2020-10-17 08:52:04 +0530

    [Rachit] Add. Dynamic Mood Change

--- MoodAnalyser/Program.cs
+++ MoodAnalyser/Program.cs
@@ -10,10 +10,10 @@ namespace MoodAnalyser
             //Console.WriteLine(mood.Analysemood());
             // MoodAnalyserFactory.CreateMoodAnalyserObject("MoodAnalyser.AnalyseMood", "AnalyseMood");
             //MoodAnalyserFactory.CreateMoodAnalyserParameterizedObject("MoodAnalyser.AnalyseMood", "AnalyseMood", "happy");
-            // MoodAnalyserFactory.InvokeAnalyserMethod("happy", "Analysemood");
-            Console.WriteLine(MoodAnalyserFactory.SetField("HAPPY", "inputMessage"));
-            AnalyseMood analyseMood = new AnalyseMood();
-            Console.WriteLine(analyseMood.inputMessage);
+            MoodAnalyserFactory.InvokeAnalyserMethod("happy", "Analysemood");
+            //Console.WriteLine(MoodAnalyserFactory.SetField("HAPPY", "inputMessage"));
+            //AnalyseMood analyseMood = new AnalyseMood();
+            //Console.WriteLine(analyseMood.inputMessage);
         }
     }
 }
