cdc8c442057a49fc0d2a600083fd11b702a576fe
Rachit Rathi <rachitrathi98@gmail.com>
2020-10-17 08:33:19 +0530

    [Rachit] Add. Dynamic Mood Change

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -7,6 +7,9 @@ namespace MoodAnalyserTest
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        /// UC 4.1: Given the mood analyser reflection return mood analyser object.
+        /// </summary>
         [TestMethod]
         public void GivenMoodAnalyserReflection_Return_MoodAnalyserObject()
         {
@@ -15,15 +18,56 @@ namespace MoodAnalyserTest
             obj.Equals(result);
         }
         /// <summary>
-        /// UC 6.1: Given happy message using reflection when proper should return happy mood
+        /// UC 4.2: Given the improper class name return mood analysis exception.
+        /// </summary>
+        [TestMethod]
+        public void GivenImproperClassName_Return_MoodAnalysisException()
+        {
+            string expected = "class not found";
+            string actual = "";
+            try
+            {
+                object result = MoodAnalyserFactory.CreateMoodAnalyserObject("MoodAnalyser.AnalyseMoods", "AnalyseMoods");
+
+            }
+            catch (MoodAnalyserCustomException e)
+            {
+                actual = e.Message;
+            }
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// UC 4.3: Given the improper constructor return mood analysis exception.
+        /// </summary>
+        [TestMethod]
+        public void GivenImproperConstructor_Return_MoodAnalysisException()
+        {
+            string expected = "constructor not found";
+            string actual = "";
+            try
+            {
+                object result = MoodAnalyserFactory.CreateMoodAnalyserObject("MoodAnalyser.AnalyseMood", "AnalyseMoods");
+
+            }
+            catch (MoodAnalyserCustomException e)
+            {
+                actual = e.Message;
+            }
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// UC 5.1: Given the mood analyser reflection return mood analyser object.
         /// </summary>
         [TestMethod]
         public void GivenMoodAnalyserReflection_Return_MoodAnalyserParameterizedObject()
         {
             var obj = new AnalyseMood("happy");
-            object result = MoodAnalyserFactory.CreateMoodAnalyserParameterizedObject("MoodAnalyser.AnalyseMood", "AnalyseMood", "happy");
+            object result = MoodAnalyserFactory.CreateMoodAnalyserParameterizedObject("TestReflections.MoodAnalyser", "MoodAnalyser", "happy");
             obj.Equals(result);
         }
+        /// <summary>
+        /// UC 6.1: Given the happy mood should return happy message.
+        /// </summary>
         [TestMethod]
         public void GivenHappyMood_ShouldReturn_HappyMessage()
         {
